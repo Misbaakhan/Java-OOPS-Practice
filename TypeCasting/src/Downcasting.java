7772a69045730daf139cf14522372130111292e8
Misbaakhan <misbakhan5922@gmail.com>
2025-10-12 17:25:53 +0530

    Downcasting.java

---
+++ TypeCasting/src/Downcasting.java
@@ -0,0 +1,43 @@
+class Animal {
+    void sound() {
+        System.out.println("animals make sound");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+       super.sound();
+    }
+
+    void Meow() {
+         System.out.println("Cat Meows");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        super.sound();
+    }
+
+    void bark() {
+        System.out.println("barking");
+    }
+}
+
+public class Downcasting {
+    public static void main(String[] args) {
+        Animal a = new Cat();
+        a.sound();
+        Cat c = (Cat) a;  //downcasting
+        c.Meow();
+        c.sound();
+        Animal a1 = new Dog();
+        a1.sound();
+        Dog d = (Dog) a1;    //downcasting
+        d.bark();
+        d.sound();
+    }
+
+}
