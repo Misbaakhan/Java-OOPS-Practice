8658118c52ce470c5b78230b24f3e9aa2ef34b3b
Misba Khanum <misbakhan5922@gmail.com>
2025-12-08 16:57:45 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,46 @@
+
+public class Encapsulation {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // public void setAge(int age) {
+    //     this.age = age;
+    // }
+
+    public Encapsulation(String name, int age) {
+        this.name = name;
+        setAge(age);
+    }
+
+    @Override
+    public String toString() {
+        return "Encapsulation [name=" + name + ", age=" + age + "]";
+    }
+    public void setAge(int age) {
+    if(age >0) {
+        this.age = age;
+    } else {
+        System.out.println("Age must be positive!");
+    }
+}
+
+    public static void main(String[] args) {
+        Encapsulation e=new Encapsulation("misba", 20);
+        System.out.println(e);
+
+    }
+
+}
