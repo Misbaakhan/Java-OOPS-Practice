84baf52a1e3301291264cb5b4ec8d07004ce9391
Misba Khanum <misbakhan5922@gmail.com>
2025-12-09 13:38:13 +0530

    Create ThrowsDemo.java

---
+++ ExceptionHandling/ThrowsDemo.java
@@ -0,0 +1,9 @@
+public class ThrowsDemo {
+    public static void main(String[] args) throws InterruptedException{
+        System.out.println("program is going to sleep for 20 seconds");
+        Thread.sleep(3000);
+        System.out.println("woke up!!");
+        
+    }
+    
+}
